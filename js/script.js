0d4dce3ef4f8a480ba6e452284baab6e3cb46f46
PAX-12 <joaquin.suarezvallejos@gmail.com>
2024-04-13 17:05:34 -0300

    comments html, css and js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-// Menú de Navegación (Sticky NavBar) y Botón de Desplazamiento (Scroll Button)
+// Menú de Navegación (Sticky NavBar) y Flecha Scroll Up (Volver a Inicio)
 let nav = document.querySelector("nav");
-let scrollBtn = document.querySelector(".scroll-button a");
+let scrollBtn = document.querySelector(".scroll-up-button a");
 console.log(scrollBtn);
 let val;
 window.onscroll = function() {
@@ -14,7 +14,7 @@ window.onscroll = function() {
 
 }
 
-// Side NavIgation Menu JS Code
+/* Menú de Navegación para Mobile (Side Navigation for Mobile) */
 let body = document.querySelector("body");
 let navBar = document.querySelector(".navbar");
 let menuBtn = document.querySelector(".menu-btn");
@@ -34,7 +34,7 @@ cancelBtn.onclick = function(){
   scrollBtn.style.pointerEvents = "auto";
 }
 
-// Side Navigation Bar Close While We Click On Navigation Links
+// Barra de Desplazamiento Lateral inactiva cuando clickeamos en links de navegación
 let navLinks = document.querySelectorAll(".menu li a");
 for (var i = 0; i < navLinks.length; i++) {
   navLinks[i].addEventListener("click" , function() {

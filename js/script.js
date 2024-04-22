13157381a04d5119b6ffd268b886f4a340d3faeb
PAX-12 <joaquin.suarezvallejos@gmail.com>
2024-04-22 01:56:22 -0300

    js script update

--- js/script.js
+++ js/script.js
@@ -51,32 +51,32 @@ contactBtn.onclick = function() {
   window.scrollTo(0, document.documentElement.scrollHeight);
 }
 
-// Menú de Navegación para Mobile (Side Navigation for Mobile)
-let body = document.querySelector("body");
-let navBar = document.querySelector(".navbar");
-let menuBtn = document.querySelector(".menu-btn");
-let cancelBtn = document.querySelector(".cancel-btn");
-menuBtn.onclick = function() {
-  navBar.classList.add("active");
-  menuBtn.style.opacity = "0";
-  menuBtn.style.pointerEvents = "none";
-  body.style.overflow = "hidden";
-  scrollBtn.style.pointerEvents = "none";
-}
-cancelBtn.onclick = function() {
-  navBar.classList.remove("active");
-  menuBtn.style.opacity = "1";
-  menuBtn.style.pointerEvents = "auto";
-  body.style.overflow = "auto";
-  scrollBtn.style.pointerEvents = "auto";
-}
+// // Menú de Navegación para Mobile (Side Navigation for Mobile)
+// let body = document.querySelector("body");
+// let navBar = document.querySelector(".navbar");
+// let menuBtn = document.querySelector(".menu-btn");
+// let cancelBtn = document.querySelector(".cancel-btn");
+// menuBtn.onclick = function() {
+//   navBar.classList.add("active");
+//   menuBtn.style.opacity = "0";
+//   menuBtn.style.pointerEvents = "none";
+//   body.style.overflow = "hidden";
+//   scrollBtn.style.pointerEvents = "none";
+// }
+// cancelBtn.onclick = function() {
+//   navBar.classList.remove("active");
+//   menuBtn.style.opacity = "1";
+//   menuBtn.style.pointerEvents = "auto";
+//   body.style.overflow = "auto";
+//   scrollBtn.style.pointerEvents = "auto";
+// }
 
-// Barra de Desplazamiento Lateral inactiva cuando clickeamos en links de navegación
-let navLinks = document.querySelectorAll(".menu li a");
-for (var i = 0; i < navLinks.length; i++) {
-  navLinks[i].addEventListener("click" , function() {
-    navBar.classList.remove("active");
-    menuBtn.style.opacity = "1";
-    menuBtn.style.pointerEvents = "auto";
-  });
-}
+// // Barra de Desplazamiento Lateral inactiva cuando clickeamos en links de navegación
+// let navLinks = document.querySelectorAll(".menu li a");
+// for (var i = 0; i < navLinks.length; i++) {
+//   navLinks[i].addEventListener("click" , function() {
+//     navBar.classList.remove("active");
+//     menuBtn.style.opacity = "1";
+//     menuBtn.style.pointerEvents = "auto";
+//   });
+// }

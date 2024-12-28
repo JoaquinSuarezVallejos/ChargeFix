a6a06d18ca16c478e447c4c66751da8890b170eb
Joaquín <joaquin.suarezvallejos@gmail.com>
2024-12-27 21:00:51 -0300

    email.php update

--- email.php
+++ email.php
@@ -1,9 +1,9 @@
 <?php
-//https://www.youtube.com/watch?v=gfuuohGgD9I
-//https://www.webhostingargentina.net/web-hosting.php
+// https://www.youtube.com/watch?v=gfuuohGgD9I
+// https://www.webhostingargentina.net/web-hosting.php
 
-//Import PHPMailer classes into the global namespace
-//These must be at the top of your script, not inside a function
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
@@ -13,7 +13,7 @@ require 'PHPMailer/Exception.php';
 require 'PHPMailer/PHPMailer.php';
 require 'PHPMailer/SMTP.php';
 
-//Create an instance; passing `true` enables exceptions
+// Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
 
 $alert = '';
@@ -30,43 +30,34 @@ if(isset($_POST['formSubmit'])){
     $myMessage = $_POST["message"];
 
     try {
-        //Server settings
-        $mail->SMTPDebug = 0;                                       //Enable verbose debug output 0: Inactivo
-        $mail->isSMTP();                                            //Send using SMTP
-        $mail->Host       = 'server9.webhostingargentina.net';      //Set the SMTP server to send through
-        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'contacto@chargefix.com.ar';            //SMTP username
-        $mail->Password   = 'MhM9DDYYRwkcyai?';                     //SMTP password
-        $mail->SMTPSecure = 'ssl';                                  //Enable implicit TLS encryption
-        $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+        // Server settings
+        $mail->SMTPDebug = 0;                                       // Enable verbose debug output 0: Inactivo
+        $mail->isSMTP();                                            // Send using SMTP
+        $mail->Host       = 'server9.webhostingargentina.net';      // Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+        $mail->Username   = 'contacto@chargefix.com.ar';            // SMTP username
+        $mail->Password   = 'MhM9DDYYRwkcyai?';                     // SMTP password
+        $mail->SMTPSecure = 'ssl';                                  // Enable implicit TLS encryption
+        $mail->Port       = 465;                                    // TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
     
-        //Recipients
-        $mail->setFrom('contacto@chargefix.com.ar', 'Chargefix');
-        $mail->addAddress('pas.suarez@gmail.com', 'Web Site');     //Add a recipient
-        //$mail->addAddress('ellen@example.com');               //Name is optional
-        //$mail->addReplyTo('info@example.com', 'Information');
-        //$mail->addCC('cc@example.com');
-        //$mail->addBCC('bcc@example.com');
+        // Set proper encoding
+        $mail->CharSet = 'UTF-8'; // Set character encoding to UTF-8
+        $mail->Encoding = 'base64'; // Use base64 encoding for content
+     
+        // Recipients
+        $mail->setFrom('contacto@chargefix.com.ar'); // Add a sender
+        $mail->addAddress('alejandro@chargefix.com.ar'); // Add a recipient
+
+        // Content
+        $mail->isHTML(true); // Set email format to HTML
+        $mail->Subject = 'Nuevo Correo | ChargeFix';
     
-        //Attachments
-        //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-        //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
-    
-        //Content
-        $mail->isHTML(true);                                  //Set email format to HTML
-        $mail->Subject = 'WebSite Contact';
-    
-        // $file = fopen("bodyEmail.html", "r");
-        // $str = fread($file, filesize("bodyEmail.html"));
-        // $str = trim($str);
-        // fclose($file);
-        //$mail->Body    = $str;
-    
+        // Build email body
         $Mybody ="
                     <html>
                         <body>
                             <div>
-                                <p>Nombre: {$myName} <br>Correo: {$myEmail} <br>Teléfono: {$myPhone}</p>
+                                <p>Nombre: {$myName} <br>Email: {$myEmail} <br>Teléfono: {$myPhone}</p>
                             </div>
                             <div>
                                 <p>Mensaje:</p>
@@ -78,10 +69,7 @@ if(isset($_POST['formSubmit'])){
     
         $mail->Body = $Mybody;
 
-
         $mail->send();
-        //debug_to_console("PhP Mailer -> Success!");
-        //$alert= "<div class='formSuccess'><span><p class='formSuccessText'>¡Mensaje enviado! Muchas gracias, en breve nos pondremos en contacto.</p></span></div>";
         $alert= "   <div class = 'formMessage'>
                         <div> <img width='80' height='80' img src='images/emailSent.png'> </div> 
                         <div> <p>Mensaje enviado, ¡muchas gracias!</p> </div> 
@@ -90,7 +78,6 @@ if(isset($_POST['formSubmit'])){
     } 
     
     catch (Exception $e) {
-        //debug_to_console("PhP Mailer -> Error!");
         $alert= "   <div class = 'formMessage'> 
                         <div> <img width='80' height='50' img src='images/emailError.png'> </div> 
                         <div> <p>Ocurrió un error al enviar el mensaje...</p> <p>Por favor, reintente más tarde.</p> </div> 

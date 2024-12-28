fbaf208bc0de41020e90607ea7e8064f1c383000
Joaquín <joaquin.suarezvallejos@gmail.com>
2024-12-27 22:12:05 -0300

    html and css changes

--- index.php
+++ index.php
@@ -7,7 +7,8 @@
     <meta charset="UTF-8" />
     <title>ChargeFix</title>
     <link rel="icon" type="image/x-icon" href="images/favicon.png">
-    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/styles.css" />
+    <link rel="stylesheet" href="css/media-queries.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" /> <!-- Fontawesome CDN Link -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   </head>
